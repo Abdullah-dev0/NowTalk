ad7ef431c391dc43b4594e7f4262fc3741985e1e
Abdullah-142 <abdulah14200@gmail.com>
2024-03-21 07:33:29 -0700

    Add Analytics component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/shared/theme-provider";
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const space_Grotesk = Space_Grotesk({ subsets: ["latin"] });
@@ -25,7 +26,10 @@ export default function RootLayout({
                enableSystem
                disableTransitionOnChange
             >
-               <main className="relative">{children}</main>
+               <main className="relative">
+                  {children}
+                  <Analytics />
+               </main>
                <Toaster position="top-center" />
             </ThemeProvider>
          </body>
