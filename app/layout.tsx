acdf9f1523edc41afc0c28604e1d01efd27c5a5b
Abdullah-142 <abdulah14200@gmail.com>
2024-12-29 14:27:55 +0500

    Update Open Graph and Twitter metadata for improved sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,51 +8,49 @@ import "./globals.css";
 const space_Grotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-   title: "NowTalk",
-   description: "A chat app for the modern web",
-   openGraph: {
-      title: "NowTalk - Chat Application",
-      description: "NowTalk is a chat application for the modern web.",
-      url: "https://now-talk-omega.vercel.app",
-      siteName: "NowTalk",
-      images: [
-         {
-            url: "/images/nowtalk.png",
-            width: 800,
-            height: 600,
-            alt: "NowTalk Preview Image",
-         },
-      ],
-   },
-   twitter: {
-      card: "summary_large_image",
-      title: "NowTalk - Chat Application",
-      description: "NowTalk is a chat application for the modern web.",
-      images: ["/images/nowtalk.png"],
-   },
+	title: "NowTalk",
+	description: "A chat app for the modern web",
+	openGraph: {
+		title: "NowTalk - Chat Application",
+		description: "NowTalk is a chat application for the modern web.",
+		url: "https://now-talk-omega.vercel.app",
+		siteName: "NowTalk",
+		images: [
+			{
+				url: "https://now-talk-omega.vercel.app/images/nowtalk.png",
+				width: 1200,
+				height: 630,
+				alt: "NowTalk Preview Image",
+			},
+		],
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "NowTalk - Chat Application",
+		description: "NowTalk is a chat application for the modern web.",
+		creator: "@Abdu_lah14", // Replace with your Twitter handle
+		site: "@Abdu_lah14", // Replace with your Twitter handle
+		images: ["https://now-talk-omega.vercel.app/images/nowtalk.png"],
+	},
 };
 
 export default function RootLayout({
-   children,
+	children,
 }: Readonly<{
-   children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-   return (
-      <html lang="en">
-         <body className={space_Grotesk.className}>
-            <ThemeProvider
-               attribute="class"
-               defaultTheme="system"
-               enableSystem
-               disableTransitionOnChange
-            >
-               <main className="relative">
-                  {children}
-                  <Analytics />
-               </main>
-               <Toaster position="top-center" />
-            </ThemeProvider>
-         </body>
-      </html>
-   );
+	return (
+		<html lang="en">
+			<body className={space_Grotesk.className}>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+					<main className="relative">
+						{children}
+						<Analytics />
+					</main>
+					<Toaster position="top-center" />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
